6a02080d08c8aba4646e3d42ea749657ae4589b0
chalupaul <me@chalupaul.com>
2021-04-11 21:40:44 -0500

    Changing publish doc link

--- src/data/config.js
+++ src/data/config.js
@@ -4,5 +4,5 @@ export const config = {
     shopDesc: "These are items you can freely buy from stores.",
     footerTitle: "Peacevale",
     footerBody: "It's Real.",
-    rulesUrl: "https://docs.google.com/document/d/1exp0gSEwffJzwG-JTOMbQCwoAQiT2UHQTVhmk-XVwxk/edit?usp=sharing"
+    rulesUrl: "https://docs.google.com/document/d/e/2PACX-1vQBNUujm0QSXCyZxCfl8okqkYxTS9Fa5jCqLg-gcdDE2oaHcEiZ2k8Pkc_ywzXYuZx47dsk4u0hbGsm/pub"
 }
